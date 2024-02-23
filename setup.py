28f1d40d59b9e302053a093faaace668225a01c6
Rasmus Thing <rasmus.thing@bestseller.com>
2024-02-23 08:04:17 +0100

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="pyfortianalyzer",
-    version="1.0.2",
+    version="2.0.0",
     description="Python API client library for Fortinet's FortiAnalyzer.",
     long_description=long_description,
     long_description_content_type="text/markdown",
